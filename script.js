4e68fa5f1fc8681f8ecc55fc71ee7d8de8576d57
EmmyBod <emmanuelolatunbosun020@gmail.com>
2023-05-31 06:07:42 -0700

    Developed a Responsive Clinical Website

---
+++ script.js
@@ -0,0 +1,33 @@
+// navbar toggling
+const navbarShowBtn = document.querySelector('.navbar_show_btn');
+const navbarCollapseDiv = document.querySelector('.navbar-collapse');
+const navbarHideBtn = document.querySelector('.navbar_hide_btn');
+
+navbarShowBtn.addEventListener('click', function(){
+    navbarCollapseDiv.classList.add('navbar-show');
+});
+navbarHideBtn.addEventListener('click', function(){
+    navbarCollapseDiv.classList.remove('navbar-show');
+});
+
+// changing search icon image on window resize
+window.addEventListener('resize', changeSearchIcon);
+function changeSearchIcon(){
+    let winSize = window.matchMedia("(min-width: 1200px)");
+    if(winSize.matches){
+        document.querySelector('.search-icon img').src = "images/search__icon.png";
+    } else {
+        document.querySelector('.search-icon img').src = "images/search__icon__dark.png";
+    }
+}
+changeSearchIcon();
+
+// stopping all animation and transition
+let resizeTimer;
+window.addEventListener('resize', () =>{
+    document.body.classList.add('resize-animation-stopper');
+    clearTimeout(resizeTimer);
+    resizeTimer = setTimeout(() => {
+        document.body.classList.remove('resize-animation-stopper');
+    }, 400);
+});
